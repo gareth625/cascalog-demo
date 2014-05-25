52d5ddc76e4a6010729dea026a5f5a1c4f01ea81
gareth625 <gareth.j.rogers@gmail.com>
2014-05-25 21:51:44 +0000

    Added a test to demo the use of Midje. It doesn't work though ;)

--- test/cam_clj/core_test.clj
+++ test/cam_clj/core_test.clj
@@ -1,6 +1,19 @@
 (ns cam-clj.core
-  (:require [cam-cli.core :refer :all]
+  (:require [cam-clj.core :refer :all]
             [cascalog.api :refer :all]
             [clojure.test :refer :all]
             [midje.cascalog :refer [produces]]
-            [midje.sweet :refer [fact tabular throws]]))
+            [midje.sweet :refer [fact tabular throws]] :reload-all))
+
+(tabular
+ (fact
+  "Midje has been extended to provide a sink for Cascalog streams, in
+  this case I'm demonstrating 'produces' sink which is used to check
+  source stream is as expected."
+  ?source-tap => (produces ?expected-data))
+
+ ; I've opted for a trivial test case again where the source is sent
+ ; straight to the sink and thus input and output is obviously the same.
+ ?source-tap              ?expected-data
+ source-tap-with-sparkles source-tap)
+
